46aa436baa72904882d9ef70dee8db7ef5290d14
MarioEstudioPC <91484233+MarioAM96@users.noreply.github.com>
2024-02-03 15:19:11 -0500

    Funcion CREATE

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import React, { useState } from 'react';
 import './App.css'; // Asegúrate de tener un archivo CSS para tus estilos
+import Axios from "axios";
 
 function App() {
 
@@ -12,8 +13,16 @@ function App() {
   const [cargo, setCargo] = useState("");
   const [anios, setAnios] = useState(0);
 
-  const mostrarDatos = ()=>{
-    alert(nombre);
+  const add = ()=>{
+    Axios.post("http://localhost:3001/create",{
+      nombre:nombre,
+      edad:edad,
+      pais:pais,
+      cargo:cargo,
+      anios:anios
+    }).then(()=>{
+      alert("Empleado registrado");
+    })
   }
 
   return (
@@ -40,7 +49,7 @@ function App() {
           <input type="number" className="inputField" onChange={(event)=>setAnios(event.target.value)} />
         </div>
         <div className="inputRow">
-          <button className="registerButton" onClick={mostrarDatos}>Registrar</button>
+          <button className="registerButton" onClick={add}>Registrar</button>
         </div>
       </div>
     </div>

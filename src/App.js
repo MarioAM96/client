b41c3664089246b8487b70c210a7f7a44aac5efc
Mario <marioandres.1996@icloud.com>
2024-02-03 13:41:53 -0500

    Styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import './App.css'; // Asegúrate de tener un archivo CSS para tus estilos
+
 function App() {
   return (
     <div className="App">
       <div className="datos">
-        <label> Nombre: <input type="text"></input></label><br/>
-        <label> Edad: <input type="text"></input></label><br/>
-        <label> Pais: <input type="text"></input></label><br/>
-        <label> Cargo: <input type="text"></input></label><br/>
-        <label> Anios: <input type="text"></input></label><br/>
+        <div className="inputRow">
+          <label>Nombre:</label>
+          <input type="text" className="inputField" />
+        </div>
+        <div className="inputRow">
+          <label>Edad:</label>
+          <input type="text" className="inputField" />
+        </div>
+        <div className="inputRow">
+          <label>País:</label>
+          <input type="text" className="inputField" />
+        </div>
+        <div className="inputRow">
+          <label>Cargo:</label>
+          <input type="text" className="inputField" />
+        </div>
+        <div className="inputRow">
+          <label>Años:</label>
+          <input type="text" className="inputField" />
+        </div>
       </div>
     </div>
   );

9789d01c4f81fc85099206b9ab101053ffaf08f3
MarioEstudioPC <91484233+MarioAM96@users.noreply.github.com>
2024-02-03 14:01:28 -0500

    Formulario terminado

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'; // Asegúrate de tener un archivo CSS para tus estilos
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [pais, setPais] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [anios, setAnios] = useState(0);
+
+  const mostrarDatos = ()=>{
+    alert(nombre);
+  }
+
   return (
     <div className="App">
       <div className="datos">
         <div className="inputRow">
           <label>Nombre:</label>
-          <input type="text" className="inputField" />
+          <input type="text" className="inputField" onChange={(event)=>setNombre(event.target.value)}/>
         </div>
         <div className="inputRow">
           <label>Edad:</label>
-          <input type="text" className="inputField" />
+          <input type="number" className="inputField" onChange={(event)=>setEdad(event.target.value)} />
         </div>
         <div className="inputRow">
           <label>País:</label>
-          <input type="text" className="inputField" />
+          <input type="text" className="inputField" onChange={(event)=>setPais(event.target.value)} />
         </div>
         <div className="inputRow">
           <label>Cargo:</label>
-          <input type="text" className="inputField" />
+          <input type="text" className="inputField" onChange={(event)=>setCargo(event.target.value)} />
         </div>
         <div className="inputRow">
           <label>Años:</label>
-          <input type="text" className="inputField" />
+          <input type="number" className="inputField" onChange={(event)=>setAnios(event.target.value)} />
+        </div>
+        <div className="inputRow">
+          <button className="registerButton" onClick={mostrarDatos}>Registrar</button>
         </div>
       </div>
     </div>
